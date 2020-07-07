d41581acafe9782ce9a11182db1ecdf565e067c5
Daniel Wilson <67807486+dcwilson26@users.noreply.github.com>
2020-07-06 18:21:17 -0700

    Update wilson.js

--- js/wilson.js
+++ js/wilson.js
@@ -172,6 +172,6 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 36.208, lng: -115.270 },
-    zoom: 12   
+    zoom: 11   
   });
 }
