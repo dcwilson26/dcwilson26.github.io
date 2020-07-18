3fa266997537939ffa99a284e6518447d9ba8d9b
Daniel Wilson <67807486+dcwilson26@users.noreply.github.com>
2020-07-17 17:53:47 -0700

    sdfads

--- highlow/highlow.js
+++ highlow/highlow.js
@@ -6,12 +6,26 @@ var max = 2
 var d = 0
 var winning = ["Mickey.gif","Office.gif","Gorilla.gif","Charlie.gif","Simon.gif","Wolf.gif","Puppy.gif","Chuck.gif","Friends.gif","GoodJob.gif","Obama.gif","Modern.gif","Joker.gif","Dogs.gif","Will.gif","Tina.gif","Minions.gif","Ace.gif","Cookie.gif","Heidi.gif","Borat.gif","Fireworks.gif","Shia.gif"]
 var arrayLength = winning.length-1
+var level = "Null"
 
 function game(x){
     $("header").fadeOut(1000);
     $("main").delay(1000).fadeIn(1000);
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+    if(x==10){
+        document.getElementById("level").innerHTML="Easy (1-10)";
+    }   else if(x==100){
+            document.getElementById("level").innerHTML="Medium (1-100)";
+        }   else if(x==1000){
+                document.getElementById("level").innerHTML="Hard (1-1,000)";
+            }   else if(x==10000){
+                    document.getElementById("level").innerHTML="Very Hard (1-10,000)";
+                }   else if(x==100000){
+                        document.getElementById("level").innerHTML="Extreme (1-100,000)";                        
+                    }   else{
+                            document.getElementById("level").innerHTML="Insane (1-1,000,000)";}
+    level = document.getElementById("level").innerHTML
     document.getElementById("label").innerHTML="Guess a number between 1 and "+formatNumber(x)+":";
     n = Math.floor(Math.random()*x)+1; 
     g = 0;
@@ -62,6 +76,7 @@ function newGame(){
     max = d;
     $("#winReset").fadeOut(500);
     $("#winImage").fadeOut(500);
+    document.getElementById("level").innerHTML=level;
     document.getElementById("left").style.display="inline-block";        
     document.getElementById("guesses").innerHTML=null;
     document.getElementById("result").innerHTML=null;
