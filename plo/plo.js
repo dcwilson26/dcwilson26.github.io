eaa4774daa8a98a90f62853d74cd8a436eac5574
Daniel Wilson <67807486+dcwilson26@users.noreply.github.com>
2020-10-13 22:27:43 -0700

    sdfsd

---
+++ plo/plo.js
@@ -0,0 +1,43 @@
+var pot = 5
+var newPot = 0
+var previous = 0
+var maxPot = 15
+var bet = 0
+
+var input = document.getElementById("bet");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13){
+        event.preventDefault();
+        document.getElementById("submit").click();   
+    }
+});
+
+function submitBet(){    
+    var list = document.createElement("li");
+    bet = document.getElementById("bet").value;
+    if(bet>maxPot || bet<=0){
+        alert("Bet not entered or exceeds pot size");
+        document.getElementById("bet").value = null;
+    }   else{
+    newPot = +pot + +previous;
+    maxPot = (+bet * 3) + +newPot;    
+    document.getElementById("pot").innerHTML = maxPot;
+    list.innerHTML = bet;
+    document.getElementById("list").appendChild(list);
+    document.getElementById("bet").value = null;  
+    pot = newPot;
+    previous = bet;    
+}}   
+
+function reset(){
+    document.getElementById("pot").innerHTML = 15;
+    var lis = document.querySelectorAll("#list li");
+    for(var i=0; li=lis[i]; i++) {
+    li.parentNode.removeChild(li);
+    }
+    pot = 5;
+    newPot = 0;
+    previous = 0;
+    maxPot = 15;
+    bet = 0;
+}
